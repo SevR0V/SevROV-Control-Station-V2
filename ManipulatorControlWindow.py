a504c57137e36c7dbe0c415119ad0b96bb6732d7
SevR0V <ermakovigorrov@gmail.com>
2024-11-17 16:56:40 +0300

    Update ManipulatorControlWindow.py

--- ManipulatorControlWindow.py
+++ ManipulatorControlWindow.py
@@ -4,5 +4,5 @@ from UI.manipulatorControlWindow import Ui_Form
 class ManipulatorControlWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Form()       # Создаем экземпляр интерфейса
-        self.ui.setupUi(self)     # Настраиваем интерфейс на данном виджете (self)
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
